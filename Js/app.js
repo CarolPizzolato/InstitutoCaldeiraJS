eb56d9be7f466302fc91543aac75d7c8973013fd
CarolPizzolato <caroline0broker0pizzolato@gmail.com>
2025-05-26 16:07:03 -0300

    quero 100 num

--- Js/app.js
+++ Js/app.js
@@ -1,18 +1,24 @@
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 0;
 let ListaSorteados  = [];
-let numLimite = 100;
+let numLimite = 10;
+
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brasilian Portuguese Female', {rate:1.2});
 }
+
 
 //ajuda o novo jogo a não ter que ficar escrevendo tudo
 function exibirMensagemInicial(){
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+
 }
+
 exibirMensagemInicial();
+
 function verificarChute() {
     let chute = document.querySelector('input').value;
     
@@ -33,17 +39,21 @@ function verificarChute() {
     }
 }
     // vamos colocar um limpar tela para cada erro ja limpar o campo
+
 function limparcampo(){
     chute = document.querySelector('input');
     chute.value = '';
 }
+
 // provavelmente vou ter que fazer o botão novo jogo
 function gerarNumeroAleatorio() {
     let numEscolhido = parseInt(Math.random() * numLimite + 1);
     let tamanho = ListaSorteados.length;
+
     if (tamanho == numLimite) {
         ListaSorteados = [];
     }
+
     if (ListaSorteados.includes(numEscolhido)){
         
         return gerarNumeroAleatorio();
@@ -52,6 +62,7 @@ function gerarNumeroAleatorio() {
         ListaSorteados.push(numEscolhido);
         return numEscolhido;}
 }
+
 function reiniciarJogo() {
     document.getElementById('reiniciar').setAttribute('disabled', true)
     numeroSecreto = gerarNumeroAleatorio();
@@ -59,3 +70,21 @@ function reiniciarJogo() {
     tentativas = 1;
     exibirMensagemInicial();
 }
+
+//vamos criar uma lista que impede repetição de valores do jogo
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
